cb6687f87b05d999a2f30ea2f199b218883b021b
eyelidlessness <gnosis@gmail.com>
2018-02-28 10:16:48 -0800

    Also try loading the model directly

--- dist/index.js
+++ dist/index.js
@@ -23,6 +23,23 @@ const test = () => __awaiter(this, void 0, void 0, function* () {
     }
     catch (error) {
         console.log('TS error', error);
+        /*
+        TS error /Users/gnosis/Projects/typeorm-bug/src/Foo.model.ts:1
+        (function (exports, require, module, __filename, __dirname) { import {
+                                                                    ^^^^^^
+
+        SyntaxError: Unexpected token import
+            at createScript (vm.js:80:10)
+            at Object.runInThisContext (vm.js:139:10)
+            at Module._compile (module.js:588:28)
+            at Object.Module._extensions..js (module.js:635:10)
+            at Module.load (module.js:545:32)
+            at tryModuleLoad (module.js:508:12)
+            at Function.Module._load (module.js:500:3)
+            at Module.require (module.js:568:17)
+            at require (internal/module.js:11:18)
+            at Function.PlatformTools.load (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/platform/PlatformTools.js:124:28)
+        */
     }
     const jsConfig = {
         type: 'postgres',
@@ -37,6 +54,50 @@ const test = () => __awaiter(this, void 0, void 0, function* () {
     }
     catch (error) {
         console.log('JS error', error);
+        /*
+        JS error { DataTypeNotSupportedError: Data type "undefined" in "Foo.bar" is not supported by "postgres" database.
+            at new DataTypeNotSupportedError (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/error/DataTypeNotSupportedError.js:16:28)
+            at /Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:68:27
+            at Array.forEach (<anonymous>)
+            at EntityMetadataValidator.validate (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:65:36)
+            at /Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:36:74
+            at Array.forEach (<anonymous>)
+            at EntityMetadataValidator.validateMany (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:36:25)
+            at Connection.buildMetadatas (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/connection/Connection.js:503:33)
+            at Connection.<anonymous> (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/connection/Connection.js:157:30)
+            at step (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/connection/Connection.js:32:23)
+        name: 'DataTypeNotSupportedError',
+        message: 'Data type "undefined" in "Foo.bar" is not supported by "postgres" database.' }
+        */
+    }
+    // Load the model directly...
+    const directConfig = {
+        type: 'postgres',
+        database: 'typeorm-bug',
+        entities: [
+            require('./Foo.model').default,
+        ],
+    };
+    try {
+        yield typeorm_1.createConnection(directConfig);
+    }
+    catch (error) {
+        console.log('Direct load error', error);
+        /*
+        Direct load error { DataTypeNotSupportedError: Data type "undefined" in "Foo.bar" is not supported by "postgres" database.
+            at new DataTypeNotSupportedError (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/error/DataTypeNotSupportedError.js:16:28)
+            at /Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:68:27
+            at Array.forEach (<anonymous>)
+            at EntityMetadataValidator.validate (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:65:36)
+            at /Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:36:74
+            at Array.forEach (<anonymous>)
+            at EntityMetadataValidator.validateMany (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:36:25)
+            at Connection.buildMetadatas (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/connection/Connection.js:503:33)
+            at Connection.<anonymous> (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/connection/Connection.js:157:30)
+            at step (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/connection/Connection.js:32:23)
+        name: 'DataTypeNotSupportedError',
+        message: 'Data type "undefined" in "Foo.bar" is not supported by "postgres" database.' }
+        */
     }
 });
 test();
--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,37 @@ const test = async () => {
         message: 'Data type "undefined" in "Foo.bar" is not supported by "postgres" database.' }
         */
     }
+
+    // Load the model directly...
+    const directConfig: PostgresConnectionOptions = {
+        type:     'postgres',
+        database: 'typeorm-bug',
+        entities: [
+            require('./Foo.model').default,
+        ],
+    };
+
+    try {
+        await createConnection(directConfig);
+    }
+    catch (error) {
+        console.log('Direct load error', error);
+        /*
+        Direct load error { DataTypeNotSupportedError: Data type "undefined" in "Foo.bar" is not supported by "postgres" database.
+            at new DataTypeNotSupportedError (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/error/DataTypeNotSupportedError.js:16:28)
+            at /Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:68:27
+            at Array.forEach (<anonymous>)
+            at EntityMetadataValidator.validate (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:65:36)
+            at /Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:36:74
+            at Array.forEach (<anonymous>)
+            at EntityMetadataValidator.validateMany (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/metadata-builder/EntityMetadataValidator.js:36:25)
+            at Connection.buildMetadatas (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/connection/Connection.js:503:33)
+            at Connection.<anonymous> (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/connection/Connection.js:157:30)
+            at step (/Users/gnosis/Projects/typeorm-bug/node_modules/typeorm/connection/Connection.js:32:23)
+        name: 'DataTypeNotSupportedError',
+        message: 'Data type "undefined" in "Foo.bar" is not supported by "postgres" database.' }
+        */
+    }
 };
 
 test();
